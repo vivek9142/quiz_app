1fc9b0245d92c8695ef4a3aa52112d1b1dd7c635
vivek9142 <vm199841@gmail.com>
2022-01-07 02:58:17 +0530

    refactored QuizQuesCard_Topic and added timer component

--- src/components/QuizQuesCard/QuizQuesCard_Topic/QuizQuesCard_Topic.component.jsx
+++ src/components/QuizQuesCard/QuizQuesCard_Topic/QuizQuesCard_Topic.component.jsx
@@ -1,73 +1,17 @@
-import { useState,useEffect,useRef} from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import {quesActions} from '../../../redux/slice/slice';
-
+import Timer from '../../Timer/Timer.component';
 import './QuizQuesCard_Topic.styles.scss';
 
 const QuizQuesCard_Topic = ({topic}) =>{   
-    const dispatch = useDispatch(); 
-    const state = useSelector(state => state.ques);
-    const currentSlide = state.currentQues;
-    const initState = { min: 0, sec: 59, timeInstance:null };
-    const [time, setTime] = useState(initState);
-    const currentTime = useRef();
-    const [domUnloading, setDomUnloading] = useState(false);
-    let [instanceTime, setInstanceTime] = useState(null);
-    let timer;
-    let timerFunc = () => {
-      timer = setInterval(() => {
-        setTime((prevState) => {
-          if (prevState.sec < 59) return { ...prevState, sec: prevState.sec + 1 };
-          else if (prevState.sec === 59 && prevState.min < 59)
-            return { ...prevState, min: prevState.min + 1, sec: 0 };
-        })
-      }, 1000);
-      
-      if(typeof instanceTime==='object')
-      setInstanceTime(timer);
-    };
-    
-    currentTime.current = time;
-    
-    useEffect(() => {
-        return () => {
-                setDomUnloading(true);
-                dispatch(quesActions.addTimeSpan(currentTime.current))
-                clearInterval(instanceTime);
-        }
-    }, [dispatch, domUnloading, instanceTime])
-    
-    
-
-    if(currentSlide===0 && typeof instanceTime==='object')
-      timerFunc();
-
-    if(domUnloading) return <></>;
-    
-    
+     
     return(
         <div className="quizQuesCard__topic--container">
-                        <div className="quizQuesCard__topic">
-                            <h3 className="quizQuesCard__topic--heading">Topic</h3>
-                            <h3 className="quizQuesCard__topic--sub-heading">{topic}</h3>
-                        </div>
-
-                        <div className="quizQuesCard__timeCounter">
-                            <div className="quizQuesCard__timeCounter--min">
-                                <span>
-                                {`${Math.floor(time.min / 10) === 0 ? "0" + time.min : time.min}`}
-                                </span>
-                                <span>min</span>
-                            </div>
-
-                            <div className="quizQuesCard__timeCounter--sec">
-                                <span>
-                                {`${Math.floor(time.sec / 10) === 0 ? "0" + time.sec : time.sec}`}
-                                </span>
-                                <span>sec</span>
-                            </div>
-                        </div>
-                    </div>
+            <div className="quizQuesCard__topic">
+                <h3 className="quizQuesCard__topic--heading">Topic</h3>
+                <h3 className="quizQuesCard__topic--sub-heading">{topic}</h3>
+            </div>
+            
+            <Timer/>
+        </div>
     );
 };
 
---
+++ src/components/Timer/Timer.component.jsx
@@ -0,0 +1,65 @@
+import { useState,useEffect,useRef} from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { quesActions } from '../../redux/slice/slice';
+
+import './Timer.styles.scss';
+
+const Timer = props => {
+    const dispatch = useDispatch(); 
+    const state = useSelector(state => state.ques);
+    const currentSlide = state.currentQues;
+    const initState = { min: 0, sec: 59, timeInstance:null };
+    const [time, setTime] = useState(initState);
+    const currentTime = useRef();
+    const [domUnloading, setDomUnloading] = useState(false);
+    let [instanceTime, setInstanceTime] = useState(null);
+    let timer;
+    let timerFunc = () => {
+      timer = setInterval(() => {
+        setTime((prevState) => {
+          if (prevState.sec < 59) return { ...prevState, sec: prevState.sec + 1 };
+          else if (prevState.sec === 59 && prevState.min < 59)
+            return { ...prevState, min: prevState.min + 1, sec: 0 };
+        })
+      }, 1000);
+      
+      if(typeof instanceTime==='object')
+      setInstanceTime(timer);
+    };
+    
+    currentTime.current = time;
+    
+    useEffect(() => {
+        return () => {
+                setDomUnloading(true);
+                dispatch(quesActions.addTimeSpan(currentTime.current))
+                clearInterval(instanceTime);
+        }
+    }, [dispatch, domUnloading, instanceTime])
+    
+    
+
+    if(currentSlide===0 && typeof instanceTime==='object')
+      timerFunc();
+
+    if(domUnloading) return <></>;
+    return (
+        <div className="quizQuesCard__timeCounter">
+            <div className="quizQuesCard__timeCounter--min">
+            <span>
+                {`${Math.floor(time.min / 10) === 0 ? "0" + time.min : time.min}`}
+            </span>
+            <span>min</span>
+        </div>
+
+        <div className="quizQuesCard__timeCounter--sec">
+            <span>
+                {`${Math.floor(time.sec / 10) === 0 ? "0" + time.sec : time.sec}`}
+            </span>
+            <span>sec</span>
+        </div>
+        </div>
+    )
+};
+
+export default Timer;
