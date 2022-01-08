652b35c2a19130c6416f1a541a082853bb91d292
vivek9142 <vm199841@gmail.com>
2022-01-09 04:19:33 +0530

    resolved one minor bug in quizQuesCard_ans component

--- src/components/QuizQuesCard/QuizQuesCard_Ans/QuizQuesCard_Ans.component.jsx
+++ src/components/QuizQuesCard/QuizQuesCard_Ans/QuizQuesCard_Ans.component.jsx
@@ -1,4 +1,4 @@
-import { useState,useRef,useEffect,useMemo } from 'react';
+import { useState,useRef,useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ButtonComp from '../../ButtonComp/ButtonComp.component';
 import { quesActions } from '../../../redux/slice/slice';
