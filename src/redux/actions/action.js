c75e4c113f35c305f465198096780cbbfef3e888
vivek9142 <vm199841@gmail.com>
2022-04-23 03:14:23 +0530

    added localstorage caching  of quiz questions on initial render

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -7,8 +7,14 @@ const getQues = createAsyncThunk(
     'Ques/fetchQuestions',
      async ()=>{
         try{
-            const response = await axios.get(url);
-            return response.data.task_array
+            const localStorageData = localStorage.getItem('data');
+            if(localStorageData)    return await JSON.parse(localStorageData);
+            else{
+                const response = await axios.get(url);
+                await localStorage.setItem('data',JSON.stringify(response.data.task_array))
+                return response.data.task_array
+            }
+            
         }catch(err){
             console.log(err)
             throw err;
